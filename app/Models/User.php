36ba9dc850e64ff239a86e3627051360c23646d0
Cyril de Wit <cyrilwit@gmail.com>
2021-11-03 13:32:39 +0100

    chore: setup Laravel application and refactor code

---
+++ app/Models/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /** @var string[] */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /** @var array */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /** @var array */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
