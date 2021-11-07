712432b4fa49f399296a57428961fe928cfa97b6
Cyril de Wit <cyrilwit@gmail.com>
2021-11-07 16:18:39 +0100

    refactor: setup email verification routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,38 @@
 <?php
 
-/** @var \Illuminate\Routing\Router $router */
+declare(strict_types=1);
+
+/** @var Router $router */
+
+use Illuminate\Routing\Router;
+use App\Http\Controllers\Auth\EmailVerificationController;
 
 $router->get('/', function () {
-    return "test";
-});
+        return "test";
+    })
+    ->name('home');
+
+// Email verification
+$router
+    ->middleware('auth')
+    ->name('email-verification.')
+    ->group(function (Router $router) {
+        $router->get('/email/verify', [EmailVerificationController::class, 'showNotice'])
+            ->name('notice');
+
+        $router->get('/email/verify/{id}/{hash}', [EmailVerificationController::class, 'verifyEmail'])
+            ->name('verify')
+            ->middleware('signed');
+
+        $router->post('/email/verification-notification', [EmailVerificationController::class, 'sendVerificationNotification'])
+            ->name('send')
+            ->middleware('throttle:6,1');
+    });
+
+// Admin portal
+$router
+    ->name('admin.')
+    ->middleware('verified')
+    ->group(function (Router $router) {
+        //
+    });
