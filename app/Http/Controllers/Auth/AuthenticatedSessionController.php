0971c93adb94f9fb5076b6d8250ff9f9c806ff61
Cyril de Wit <cyrilwit@gmail.com>
2021-11-17 13:44:32 +0100

    feat: add authenticated session controller

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\Factory as ViewFactory;
+
+class AuthenticatedSessionController extends Controller
+{
+    private ViewFactory $viewFactory;
+
+    public function __construct(ViewFactory $viewFactory)
+    {
+        $this->viewFactory = $viewFactory;
+    }
+
+    /**
+     * Display the login view.
+     */
+    public function create(): View
+    {
+        return $this->viewFactory->make('auth.login');
+    }
+
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): RedirectResponse
+    {
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        return redirect()->intended(RouteServiceProvider::HOME);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
