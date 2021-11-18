58c68d8b9a17ca7c9ebae5ca06a2d881a3371a06
Cyril de Wit <cyrilwit@gmail.com>
2021-11-18 14:20:54 +0100

    feat: reset password feature

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,11 @@
 namespace App\Models;
 
 use App\Models\Concerns\HasUuid;
+use App\Notifications\Auth\ResetPassword;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContact;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,13 +15,14 @@ use Illuminate\Support\Collection;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable implements MustVerifyEmail, CanResetPasswordContact
 {
     use HasApiTokens,
         HasFactory,
         Notifiable,
         HasUuid,
-        HasRoles;
+        HasRoles,
+        CanResetPassword;
 
     /** @var string[] */
     protected $fillable = [
@@ -45,8 +50,6 @@ class User extends Authenticatable implements MustVerifyEmail
             $this->last_name
         ]));
 
-        dump($nameParts->toArray());
-
         return $nameParts
             ->filter(function ($part) {
                 return !empty($part);
@@ -61,5 +64,19 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         throw new Exception('This feature is not created yet.');
 //        $this->notify(new VerifyEmail);
+    }
+
+    /**
+     * Send the password reset notification.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPassword(
+            resolve(UrlGenerator::class),
+            $token
+        ));
     }
 }
