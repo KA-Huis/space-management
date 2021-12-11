992e5a6b9573c1db64f7e1b80e0093cdb3856e86
Cyril de Wit <cyrilwit@gmail.com>
2021-12-11 23:51:42 +0100

    chore: improve fixer config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -3,14 +3,26 @@
 declare(strict_types=1);
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__ . '/src')
-    ->in(__DIR__ . '/tests');
+    ->in([
+        __DIR__ . '/app',
+        __DIR__ . '/config',
+        __DIR__ . '/database',
+        __DIR__ . '/resources',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+    ])
+    ->name('*.php')
+    ->notName('*.blade.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
 
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
         'ordered_imports' => true,
-        'concat_space' => ['spacing' => 'one'],
+        'concat_space' => [
+            'spacing' => 'one',
+        ],
         'yoda_style' => false,
     ])
     ->setFinder($finder);
