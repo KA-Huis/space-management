dcb25312a4c25ddb333f0e46685e5bb48c9f4a65
cyrildewit <cyrildewit@users.noreply.github.com>
2022-04-07 14:10:27 +0000

    style: fix code style

--- app/Models/ApiUser.php
+++ app/Models/ApiUser.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
 class ApiUser extends Authenticatable
