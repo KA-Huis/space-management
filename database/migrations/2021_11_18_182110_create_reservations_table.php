d8997c494d86cb23ab7b54df8678c59595e74f9b
cyrildewit <cyrildewit@users.noreply.github.com>
2022-04-14 10:34:26 +0000

    style: fix code style

--- database/migrations/2021_11_18_182110_create_reservations_table.php
+++ database/migrations/2021_11_18_182110_create_reservations_table.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-use App\Models\User;
 use App\Models\Group;
 use App\Models\Space;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
