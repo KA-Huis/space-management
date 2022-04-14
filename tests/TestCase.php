ffed935ef5a0caeef7866f63271b3aae96a14781
Cyril de Wit <cyrilwit@gmail.com>
2022-04-14 17:54:06 +0200

    SM-55: add assertJsonPaginated method to TestResponse, so we can easily check if the response is a JsonPaginated response

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,8 +5,22 @@ declare(strict_types=1);
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Testing\TestResponse;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        TestResponse::macro('assertJsonPaginated', function () {
+            return $this->assertJson([
+                'data' => true,
+                'links' => true,
+                'meta' => true,
+            ]);
+        });
+    }
 }
