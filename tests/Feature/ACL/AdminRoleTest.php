b244d0d15ed291f64f5ed10e395c7c5ba3fd4195
Cyril de Wit <cyrilwit@gmail.com>
2022-04-26 13:47:49 +0200

    SM-79: implicitly grant "Admin" role all permissions

---
+++ tests/Feature/ACL/AdminRoleTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\ACL;
+
+use App\ACL\Roles\AdminRole;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Gate;
+use Spatie\Permission\Models\Role;
+use Tests\TestCase;
+
+class AdminRoleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testGateBeforeHookToImplicitlyGrantAdminRoleAllPermissions(): void
+    {
+        // Given
+        Role::findOrCreate((new AdminRole)->getName());
+
+        $user = User::factory()->create();
+        $user->assignRole((new AdminRole)->getName());
+
+        // When
+
+        $response = Gate::forUser($user)->allows('something random that does not exists', User::class);
+
+        // Then
+        self::assertTrue($response);
+    }
+}
